44c1039a8f01a85fb5680da2405cf0187d3ef15a
Dor Friedman <df9sv@virginia.edu>
2015-11-09 03:23:20 -0500

    code

---
+++ code/python/hs.py
@@ -0,0 +1,12 @@
+def hs(n)
+    n
+    while (n > 1)
+        x = 0
+        if (n != 1)
+            if (n % 2 == 0)
+                n = n//2
+            else:
+                n = 3*n + 1
+        else:
+            1
+        x = x + 1
